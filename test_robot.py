edb221b7d8e0e5eb01dbd6648345e69d95227321
Gavmeister <GavinMunro@users.noreply.github.com>
2017-04-07 23:36:47 +1000

    Unit testing

---
+++ test_robot.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+__author__ = 'Gavin A.I. Munro'
+
+
+class TestRobot(TestCase):
+    pass
+
+
+class TestRobot(TestCase):
+    def test_place(self):
+        self.fail()
+
+    def test_on_table(self):
+        self.fail()
+
+    def test_move(self):
+        self.fail()
+
+    def test_right(self):
+        self.fail()
+
+    def test_left(self):
+        self.fail()
+
+    def test_report(self):
+        self.fail()
